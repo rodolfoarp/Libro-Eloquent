8a2e3f1299e3aa80910f930ace5eb02d67cf5e2b
Rodolfo Ramirez <ramirezrodolfo333@gmail.com>
2021-03-17 23:29:30 -0430

    Lista con JavaScripts

--- src/ejerciciosEloquent/ejercicio11.js
+++ src/ejerciciosEloquent/ejercicio11.js
@@ -1,16 +1,19 @@
-function arrayLista(array){
-    const lista = {};
-    if (array.length <= 0){
-        return lista;
-    } else if (array.length === 1){
-        lista.valor == array.shift();
-        lista.resto = null;
-        return lista;
-    } else {
-        lista.valor = array.shift();
-        lista.resto = arrayLista(array);
+function arrayToList(array){ 
+    let list = null
+    for(let i = array.length - 1; i >= 0 ; i--){
+        list = { value: array[i], rest: list}
+    };
+    return list;
+};
+
+function listToArray(list){
+    let array = [];
+    for(let nodo = list; nodo; nodo = nodo.rest){
+        array.push(nodo.value);
     }
-    return lista;
+    return array
 }
 
-console.log(arrayLista([1,2,3]))
+console.log(arrayToList([10,20,30]))
+console.log(listToArray(arrayToList([10,20,30])))
+
